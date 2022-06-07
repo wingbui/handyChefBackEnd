430c6a55f287727a601297e93b841166153965a4
Simer <singhsimer09@gmail.com>
2022-06-07 09:50:42 -0700

    someBookingModelTweaks

--- models/Booking.js
+++ models/Booking.js
@@ -5,15 +5,22 @@ const bookingSchema = new mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: 'User',
   },
+
   chefService: {
     type: mongoose.Schema.ObjectId,
     ref: 'ChefService',
   },
+
   bookingDate: { type: Date },
+
   numberOfCustomers: { type: Number },
+
   selectedDish: [{ type: mongoose.Schema.ObjectID, reference: 'Dish ' }],
+
   notes: { type: String },
+
   totalPrice: { type: Number },
+
   status: {
     type: String,
     enum: ['todo', 'cancelled', 'done'],
