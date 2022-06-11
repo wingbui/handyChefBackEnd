fcf84819e7bb680be01b7d5958482e3e9d43d59a
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-06-11 10:33:54 -0700

    done add favorite

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+const User = require('../models/User.js');
+
+const toggleAddRemoveFavoriteChef = async (req, res, next) => {
+  const { chefService } = req.body;
+  if (!chefService) {
+    next(new Error('Please pass in a chef service ObjectId'));
+    return;
+  }
+
+  try {
+    let currentUser = await User.findById(req.user._id);
+
+    if (
+      currentUser.favoriteChefs.length &&
+      currentUser.favoriteChefs.length > 0
+    ) {
+      if (currentUser.favoriteChefs.find((item) => item.equals(chefService))) {
+        currentUser.favoriteChefs = currentUser.favoriteChefs.filter(
+          (item) => !item.equals(chefService)
+        );
+        await currentUser.save();
+        res.json({ user: currentUser });
+      } else {
+        currentUser.favoriteChefs.push(chefService);
+        await currentUser.save();
+        res.json({ user: currentUser });
+      }
+    } else {
+      currentUser.favoriteChefs.push(chefService);
+      await currentUser.save();
+      res.json({ user: currentUser });
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports = { toggleAddRemoveFavoriteChef };
