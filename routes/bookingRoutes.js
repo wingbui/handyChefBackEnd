0b15be4b2b3ff9abe7569aa0c94f282304f81e7c
Simer <singhsimer09@gmail.com>
2022-06-17 10:35:52 -0700

    doneWithGettingBookingById

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,19 +1,23 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 const {
   createBooking,
   getAllBookings,
   getAllBookingsForChef,
-} = require('../controllers/bookingController')
+  getBooking,
+} = require("../controllers/bookingController")
 
-const { authenticateUser, restrictedTo } = require('../middleware/auth')
-
-router
-  .route('/')
-  .post(authenticateUser, restrictedTo('customer'), createBooking)
-  .get(authenticateUser, restrictedTo('customer'), getAllBookings)
+const { authenticateUser, restrictedTo } = require("../middleware/auth")
 
 router
-  .route('/chef')
-  .get(authenticateUser, restrictedTo('chef'), getAllBookingsForChef)
+  .route("/")
+  .post(authenticateUser, restrictedTo("customer"), createBooking)
+  .get(authenticateUser, restrictedTo("customer"), getAllBookings)
+
+router
+  .route("/chef")
+  .get(authenticateUser, restrictedTo("chef"), getAllBookingsForChef)
+
+router.route("/:id").get(authenticateUser, getBooking)
+
 module.exports = router
