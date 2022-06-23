a2da21f8ecd906b44d7121e60bf3a3b2cab80985
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-06-23 09:43:18 -0700

    add: pushNotificationToken property to User model

--- models/User.js
+++ models/User.js
@@ -46,6 +46,9 @@ const UserSchema = new mongoose.Schema({
     },
   ],
   preferredCuisine: [{ type: String }],
+  pushNotificationToken: {
+    type: String,
+  },
 });
 
 UserSchema.pre('save', async function () {
