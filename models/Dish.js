6d9b8fcb6afd3acfa05a604b79e2005b6d7b1d58
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-06-30 13:19:56 -0700

    modify Dish model

--- models/Dish.js
+++ models/Dish.js
@@ -12,6 +12,20 @@ const DishSchema = new mongoose.Schema({
   chefService: {
     type: mongoose.Schema.ObjectId,
     ref: 'ChefService',
+    required: true,
+  },
+  cuisine: {
+    type: [{ type: String }],
+    validate: {
+      validator: (v) => {
+        if (v.length < 1) {
+          return false;
+        } else if (v.includes('')) {
+          return false;
+        }
+      },
+      message: 'Please pass in cuisine type for this dish',
+    },
   },
 });
 
