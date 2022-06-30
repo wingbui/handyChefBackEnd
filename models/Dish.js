fa3a1eb97d1d97a4b72d032e12bef81b290af6de
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-06-30 12:41:50 -0700

    refactor: remove empty lines in an objet

--- models/Dish.js
+++ models/Dish.js
@@ -1,20 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const DishSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-
   price: {
     type: Number,
     required: true,
   },
-
   chefService: {
     type: mongoose.Schema.ObjectId,
     ref: 'ChefService',
   },
-})
+});
 
-module.exports = mongoose.model('Dish', DishSchema)
+module.exports = mongoose.model('Dish', DishSchema);
