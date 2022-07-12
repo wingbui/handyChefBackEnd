12b5ed5c7cafb613ee47da0872e9d5ae9c89c236
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-12 10:40:03 -0700

    add: message for notification

--- models/Notification.js
+++ models/Notification.js
@@ -15,6 +15,10 @@ const NotificationSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  message: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Notification', NotificationSchema);
