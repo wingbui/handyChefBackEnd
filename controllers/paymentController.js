976a3ddd882c4a0881003ee5cc4732e889b9eeec
Hiroshi Isobe <isobe69b@gmail.com>
2022-07-27 17:23:24 -0700

    Build: Remove name property and make the total amount dynamic

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,33 +1,21 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const doPayment = async (req, res) => {
   try {
-    const {name} = req.body;
-    if(!name) return res.status(400).json({ message: 'Please enter a name' });
+    const { amount } = req.body;
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: Math.round(25 * 100),
+      amount: amount * 100,
       currency: 'CAD',
-      payment_method_types: ["card"],
-      metadata: {name}
+      payment_method_types: ['card'],
     });
     const clientSecret = paymentIntent.client_secret;
     res.json({ message: 'Payment initiated', clientSecret });
-  } catch(err) {
+  } catch (err) {
     console.log(err);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
 
 module.exports = { doPayment };
-
-// Pasted from the web article
-/* return stripe.charges
-  .create({
-    amount: req.body.amount,
-    currency: req.body.currency,
-    source: req.body.tokenId,
-    description: req.body.description,
-  })
-  .then((result) => res.status(200).json(result)); */
